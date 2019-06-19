ea7f94827ae7d4204f717708f5c2f6ae98809efd
Administrator <skyestzhang@163.com>
2019-06-19 14:50:51 +0800

    .

---
+++ fds.js
@@ -0,0 +1,142 @@
+let a = {
+    program: '一个程序',
+    chNR: 'abc',
+    time: '2019.02.01 12:20:51',
+    tempUnit: 0,
+    programData: [
+        {
+            mode: 0,
+            event: 2,
+            cabinetTemp: '32',
+            coreTempTarget: '?',
+            coreTemp: '100',
+            time: '20190205',
+            tempUnit: 'c',
+            energyOpt: '1',
+            energyHalf: '1'
+        },
+        {
+            mode: 0,
+            event: 2,
+            cabinetTemp: '32',
+            coreTempTarget: '?',
+            coreTemp: '100',
+            time: '20190205',
+            tempUnit: 'c',
+            energyOpt: '1',
+            energyHalf: '1'
+        },
+        {
+            mode: 0,
+            event: 2,
+            cabinetTemp: '32',
+            coreTempTarget: '?',
+            coreTemp: '100',
+            time: '20190205',
+            tempUnit: 'c',
+            energyOpt: '1',
+            energyHalf: '1'
+        },
+        {
+            mode: 0,
+            event: 2,
+            cabinetTemp: '32',
+            coreTempTarget: '?',
+            coreTemp: '100',
+            time: '20190205',
+            tempUnit: 'c',
+            energyOpt: '1',
+            energyHalf: '1'
+        },
+        {
+            mode: 0,
+            event: 2,
+            cabinetTemp: '32',
+            coreTempTarget: '?',
+            coreTemp: '100',
+            time: '20190205',
+            tempUnit: 'c',
+            energyOpt: '1',
+            energyHalf: '1'
+        },
+        {
+            mode: 0,
+            event: 2,
+            cabinetTemp: '32',
+            coreTempTarget: '?',
+            coreTemp: '100',
+            time: '20190205',
+            tempUnit: 'c',
+            energyOpt: '1',
+            energyHalf: '1'
+        },
+        {
+            mode: 0,
+            event: 2,
+            cabinetTemp: '32',
+            coreTempTarget: '7',
+            coreTemp: '100',
+            time: '20190205',
+            tempUnit: 'c',
+            energyOpt: '1',
+            energyHalf: '1'
+        },
+        {
+            mode: 0,
+            event: 2,
+            cabinetTemp: '32',
+            coreTempTarget: '8',
+            coreTemp: '100',
+            time: '20190205',
+            tempUnit: 'c',
+            energyOpt: '1',
+            energyHalf: '1'
+        },
+        {
+            mode: 0,
+            event: 2,
+            cabinetTemp: '32',
+            coreTempTarget: '?',
+            coreTemp: '100',
+            time: '20190205',
+            tempUnit: 'c',
+            energyOpt: '1',
+            energyHalf: '1'
+        },
+        {
+            mode: 0,
+            event: 2,
+            cabinetTemp: '32',
+            coreTempTarget: '?',
+            coreTemp: '100',
+            time: '20190205',
+            tempUnit: 'c',
+            energyOpt: '1',
+            energyHalf: '1'
+        },
+        {
+            mode: 0,
+            event: 2,
+            cabinetTemp: '32',
+            coreTempTarget: '?',
+            coreTemp: '100',
+            time: '20190205',
+            tempUnit: 'c',
+            energyOpt: '1',
+            energyHalf: '1'
+        },
+        {
+            mode: 0,
+            event: 2,
+            cabinetTemp: '32',
+            coreTempTarget: '12',
+            coreTemp: '100',
+            time: '20190205',
+            tempUnit: 'c',
+            energyOpt: '1',
+            energyHalf: '1'
+        }
+
+    ]
+}
+console.log(JSON.stringify(a))
---
+++ tetst.js
@@ -0,0 +1,4 @@
+let a = [ 1,2,3,4,5,6,4,7,8,9,7,10]
+let r  = a.slice(2,2+5)
+
+console.log(r)
